faaba2afb2b7d5c7df849f9effde4606f73849e9
Tomislav Maric <tomislav.maric@gmx.com>
2023-11-12 13:28:35 +0100

    [foamSmartSimSvd] writing all fields to smartredis

---
+++ postProcessing/foamSmartSimSvd/foamSmartSimSvd/foamSmartSimSvd.C
@@ -0,0 +1,123 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2023 
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    foamSmartSimSvd
+
+Description
+
+    Uses SmartSim/SmartRedis for computing Singular Value Decomposition 
+    of OpenFOAM fields.
+
+\*---------------------------------------------------------------------------*/
+
+#include "error.H"
+#include "fvCFD.H"
+#include "wordList.H"
+#include "timeSelector.H"
+#include "client.h"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    // Selecting the time step folders for the approximated fields.
+    timeSelector::addOptions();
+
+    // Add the option to the application for the name of the approximated field.
+    argList::addOption
+    (
+        "fieldName", 
+        "fieldName",
+        "Name of the approximated field, e.g. p." 
+    ); 
+
+    // OpenFOAM boilerplate: set root folder and options
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    // Read the name of the approximated field from the command line option
+    // foamSmartSimDmd -case cavity -fieldName p
+    const word fieldName = args.get<word>("fieldName");
+    Info << "Approximating field " << fieldName << endl;
+
+    SmartRedis::Client smartRedisClient(false);
+
+    // Create a list of all time step folders from the case folder.
+    instantList inputTimeDirs = timeSelector::select0(runTime, args);
+
+    forAll(inputTimeDirs, timeI)
+    {
+        const auto& currentTime = inputTimeDirs[timeI];
+
+        #include "createFields.H"
+
+        // If 'field' is a volScalarField 
+        if (!inputVolScalarFieldTmp->empty())
+        {
+            // Create the cell centers DataSet
+            const auto mpiIndexStr = std::to_string(Pstream::myProcNo());
+
+            const auto redisFieldName = "field_" + fieldName + 
+                                        "_time_" + currentTime.name() +
+                                        "_MPIrank_" + mpiIndexStr; 
+
+            auto redisDatasetName = "dataset_" + redisFieldName; 
+            SmartRedis::DataSet redisDataset(redisDatasetName);
+
+            // Add the type name into the input field dataset metadata. 
+            redisDataset.add_meta_string("type", "scalar");
+
+            //- Put the input field in smartredis.
+            Pout << "Writing field " << redisFieldName 
+                << " to SmartRedis. " << endl;
+
+            redisDataset.add_tensor(redisFieldName,
+                                    (void*)inputVolScalarFieldTmp->cdata(), 
+                                    std::vector<size_t>{size_t(mesh.nCells()), 1},
+                                    SRTensorTypeDouble, SRMemLayoutContiguous);
+
+            smartRedisClient.put_dataset(redisDataset);
+            smartRedisClient.append_to_list("redisDatasetList", 
+                                             redisDataset);
+        }
+        // TODO(TM): same as for the volScalarField only with vector dimensions.
+        else if (!inputVolVectorFieldTmp->empty())
+        {
+        }
+    }
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    
+    Info<< nl;
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
