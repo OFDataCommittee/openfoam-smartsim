1707489bd77595010c5020dcf02a1a067d281d8b
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2023-11-19 00:13:46 +0100

    refactor(fo): major overhaul of the database-openfoam interaction interface

---
+++ src/smartRedis/smartRedisDatabase/smartRedisDatabase.C
@@ -0,0 +1,226 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2019-2021 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "Ostream.H"
+#include "smartRedisDatabase.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "dictionary.H"
+#include "Time.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(smartRedisDatabase, 0);
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::smartRedisDatabase::smartRedisDatabase
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    name_(name),
+    region_(dict.getOrDefault<word>("region", polyMesh::defaultRegion)),
+    mesh_(runTime.lookupObject<fvMesh>(region_)),
+    clientName_(dict.getOrDefault<word>("clientName", "default")),
+    redisDB_(
+        runTime.foundObject<smartRedisAdapter>(clientName_)
+        ? &runTime.lookupObjectRef<smartRedisAdapter>(clientName_)
+        : new smartRedisAdapter
+            (
+                IOobject
+                (
+                    clientName_,
+                    runTime.timeName(),
+                    runTime,
+                    IOobject::NO_READ,
+                    IOobject::AUTO_WRITE
+                ),
+                dict
+            )
+    ),
+    namingConvention_(),
+    namingConventionState_()
+{
+    namingConvention_.set("dataset", wordList{"time_index", "mpi_rank"});
+    namingConvention_.set("field", wordList{"name", "patch"});
+    updateNamingConventionState();
+    dictionary meta = postMetadata();
+    if (debug)
+    {
+        Info
+            << "The following scheme is adopted as a naming convention:"
+            << meta << endl;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void
+Foam::smartRedisDatabase::updateNamingConventionState()
+{
+    dictionary values;
+    values.set<string>("time_index", Foam::name(mesh().time().timeIndex()));
+    values.set<string>("mpi_rank", Foam::name(Pstream::myProcNo()));
+    values.set<string>("name", "+");
+    values.set<string>("patch", "internal");
+    namingConventionState_ = createSchemeValues(values);
+}
+
+Foam::dictionary
+Foam::smartRedisDatabase::postMetadata()
+{
+    dictionary meta;
+    if (Pstream::master() && this->namingConvention_.toc().size() > 0) {
+        word dsName = name()+"_metadata";
+        // This will override the dataset if it exists
+        DataSet ds(dsName);
+        forAllConstIter(HashTable<wordList>, namingConvention_, i) {
+            const wordList& args = *i;
+            word tmplt = (i.key() == "dataset" )? i.key() : name();
+            forAll(args, e) {
+                tmplt += "_" + args[e] + "_{{" + args[e] + "}}";
+            }
+            ds.add_meta_string(i.key(), tmplt);
+            meta.set<string>(i.key(), tmplt);
+        }
+        redisDB_->client().put_dataset(ds);
+    }
+    return meta;
+}
+
+DataSet
+Foam::smartRedisDatabase::getMetadata()
+{
+    word dsName = name()+"_metadata";
+    if (!redisDB_->client().dataset_exists(dsName))
+    {
+        FatalErrorInFunction
+            << "Metadata dataset: " << dsName
+            << " does not exist in SmartRedis database."
+            << nl << abort(FatalError);
+    }
+    return redisDB_->client().get_dataset(dsName);
+}
+
+Foam::dictionary
+Foam::smartRedisDatabase::createSchemeValues
+(
+    const dictionary& values,
+    bool allowIncomplete
+) const
+{
+    dictionary schemeVals;
+    forAllConstIter(HashTable<wordList>, namingConvention(), i)
+    {
+        dictionary subVals;
+        word key = i.key();
+        const wordList& args = *i;
+        // @todo: Better error handling when creating scheme values
+        // @body: If the user does not provide all the arguments for a name
+        //        convention; the code will crash. We need to provide a better
+        //        error message than `Entry '<something>' not found in dictionary ""`
+        for(auto& arg: args) {
+            if (allowIncomplete) {
+                if (values.found(arg))
+                {
+                    subVals.set<string>(arg, word(values.lookup(arg)));
+                } else {
+                    subVals.set<string>(arg, "");
+                }
+            } else {
+                subVals.set<string>(arg, word(values.lookup(arg)));
+            }
+        }
+        schemeVals.set(key, subVals);
+    }
+    return schemeVals;
+}
+
+Foam::word
+Foam::smartRedisDatabase::extractName
+(
+    const word key,
+    const dictionary& schemeValue
+) const
+{
+    word nameDB = (key == "dataset") ? name() : key;
+    const dictionary args = schemeValue.subDict(key);
+    for(const auto& arg: namingConvention()[key])
+    {
+       nameDB += "_" + arg + "_" + word(args.lookup(arg));
+    }
+    return nameDB;
+}
+
+Foam::word
+Foam::smartRedisDatabase::getDBFieldName
+(
+    const word fName
+) const
+{
+    dictionary fieldConvention = namingConventionState_;
+    fieldConvention.subDict("field").set<string>("patch", "internal");
+    fieldConvention.subDict("field").set<string>("name", fName);
+    auto dt = extractName("dataset", fieldConvention);
+    auto ft = extractName("field", fieldConvention);
+    return word("{") + dt + "}." + ft;
+}
+
+void
+Foam::smartRedisDatabase::sendGeometricFields
+(
+    const wordList& fieldNames
+)
+{
+    updateNamingConventionState();
+    word dsName = extractName("dataset", namingConventionState_);
+    DataSet ds(dsName);
+    sendAllFields<supportedFieldTypes>(ds, fieldNames);
+    client().put_dataset(ds);
+}
+
+void
+Foam::smartRedisDatabase::getGeometricFields
+(
+    const wordList& fileNames
+)
+{
+    //@todo: Implement `getGeometricFields` method
+    //@body: This is supposed to get corresponding tensors from the Database and override OpenFOAM fields with them.
+    NotImplemented;
+}
+
+// ************************************************************************* //
